545ad63c1c02f826914888cbf085452164ac31f6
shareef-sanaullah <57405273+shareef-sanaullah@users.noreply.github.com>
2019-11-19 12:25:03 +0300

    Add files via upload

---
+++ Batch6_PY_Day7.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+alien = {'color':'green','points':5}
+print(alien)
+
+
+# In[22]:
+
+
+print(alien['color'])
+
+
+# In[23]:
+
+
+new_points = alien['points']
+
+
+print(f"you just earned {new_points} points.")
+
+
+# In[ ]:
+
+
+recommender system:
+
+
+# In[29]:
+
+
+
+
+facebook_1 = {}
+
+facebook_1['user1'] = 'f1'
+alien_1['fname'] = 'tony'
+alien_1['lname'] = 'stark'
+
+print(alien_1)
+
+
+# In[ ]:
+
+
+
+
